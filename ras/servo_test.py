198082bd861cdc98256e77c3b14f9b121f2ebc04
Yubo-Cao <cao2006721@gmail.com>
2023-07-05 10:07:20 -0400

    feat: modify the servo test

--- ras/servo_test.py
+++ ras/servo_test.py
@@ -1,12 +1,19 @@
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+from guizero import App, Slider, Text
+import numpy as np
 
-factory = PiGPIOFactory(host='192.168.6.105')
-servo = Servo(17,min_pulse_width=0.8/1000, max_pulse_width=2.2/1000,pin_factory=factory)
 
-while True:
-    servo.min()
-    sleep(0.1)
-    servo.max()
-    sleep(0.1)
+factory = PiGPIOFactory(host="192.168.137.191")
+servo = Servo(
+    17,
+    min_pulse_width=1.0 / 1000,
+    max_pulse_width=2.0 / 1000,
+    pin_factory=factory,
+)
+
+
+for i in np.arange(-1, 1, 0.1):
+    servo.value = i
+    sleep(0.2)
