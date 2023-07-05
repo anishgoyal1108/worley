37294b337d7b85485de43f0e6725d30c30a9e8da
Anish G <90469168+anishgoyal1108@users.noreply.github.com>
2023-07-05 11:08:38 -0400

    feat: added a reflection

--- ras/servo_test.py
+++ ras/servo_test.py
@@ -2,11 +2,11 @@ from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-factory = PiGPIOFactory(host='192.168.6.105')
+factory = PiGPIOFactory(host='192.168.137.191')
 servo = Servo(17,min_pulse_width=0.8/1000, max_pulse_width=2.2/1000,pin_factory=factory)
 
 while True:
     servo.min()
-    sleep(0.1)
+    sleep(2)
     servo.max()
-    sleep(0.1)
+    sleep(2)
