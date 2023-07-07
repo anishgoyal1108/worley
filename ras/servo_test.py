7ddb54a2e4598d3a2c39ce0b6d2ac184da719273
Yubo Cao <cao2006721@gmail.com>
2023-07-07 09:13:09 -0400

    feat: fix the servo

--- ras/servo_test.py
+++ ras/servo_test.py
@@ -1,12 +1,19 @@
-from gpiozero import Servo
+from gpiozero import AngularServo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-factory = PiGPIOFactory(host='192.168.137.191')
-servo = Servo(17,min_pulse_width=0.8/1000, max_pulse_width=2.2/1000,pin_factory=factory)
+factory = PiGPIOFactory()
+servo = AngularServo(
+    17,
+    min_angle=0,
+    max_angle=180,
+    min_pulse_width=0.0005,
+    max_pulse_width=0.0025,
+    pin_factory=factory,
+)
 
 while True:
-    servo.min()
-    sleep(2)
-    servo.max()
-    sleep(2)
+    servo.angle = 0
+    sleep(1)
+    servo.angle = 180
+    sleep(1)
