1df55c5dabb5247311c6d516a92f5dac3dcdcc27
Yubo Cao <cao2006721@gmail.com>
2023-07-07 09:57:33 -0400

    refact(ras): move the servo_test to main

--- ras/main.py
+++ ras/main.py
@@ -1,10 +1,20 @@
 from gpiozero import AngularServo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+import json_schema
+import logging
+from pathlib import Path
+
+log = logging.getLogger()
+
+
+def load_config():
+    ...
+
 
 factory = PiGPIOFactory()
 servo = AngularServo(
-    17,
+    3,
     min_angle=0,
     max_angle=180,
     min_pulse_width=0.0005,
