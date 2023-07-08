7561046a88da982761900c6e1f2488f3091b95ae
Yubo Cao <cao2006721@gmail.com>
2023-07-08 11:02:05 -0400

    feat: add small test to act out each letter

--- ras/main.py
+++ ras/main.py
@@ -4,10 +4,11 @@ import re
 from functools import partial
 from pathlib import Path
 from time import sleep
+from string import ascii_uppercase
 
 import yaml
-from adafruit_servokit import ServoKit
 from jsonschema import validate
+from typing import Literal
 from rich import print
 from rich.logging import RichHandler
 
@@ -32,13 +33,16 @@ def load_config():
 class ServoController:
     """Controls the servos."""
 
-    def __init__(self, config):
+    def __init__(self, config, type: Literal["adafruit", "gpiozero"] = "gpiozero"):
         log.info("Initializing controller...")
         self.config = config
 
         self.word = None
         self._transitions = self._load_transitions()
-        self._servos = self._load_servos()
+        if type == "adafruit":
+            self._servos = self._load_adafruit_servos()
+        else:
+            self._servos = self._load_gpiozero_servos()
         self._words = self.config["words"]
 
     def act(self, word: str):
@@ -55,7 +59,10 @@ class ServoController:
 
     def _act_raw(self, servo_config: dict[str, int]):
         for name, angle in servo_config.items():
-            self._servos[name].angle = angle
+            if servo := self._servos.get(name):
+                servo.angle = angle
+            else:
+                log.warning(f'Servo {name} is not supported')
 
     def _transition(self, from_: str, to: str, dt=0.05):
         log.debug(f"Transitioning from {from_} to {to}...")
@@ -73,17 +80,37 @@ class ServoController:
         log.info("Loading transitions...")
         return {
             (transition["from"], transition["to"]): transition
-            for transition in self.config["transitions"]
+            for transition in self.config.get("transitions", [])
         }
 
-    def _load_servos(self):
+    def _load_adafruit_servos(self):
+        from adafruit_servokit import ServoKit
+                
         log.info("Initializing servos...")
         self._kit = ServoKit(channels=16)
         return {
             name: self._kit.servo[pin] for name, pin in self.config["servos"].items()
+            if pin in range(0, 15) # support omitted servos
         }
-
+    
+    def _load_gpiozero_servos(self):
+        from gpiozero import AngularServo
+        from gpiozero.pins.pigpio import PiGPIOFactory
+        
+        log.info("Initializing servos...")
+        self._factory = PiGPIOFactory()
+        make_servo = partial(
+            AngularServo,
+            min_angle=0,
+            max_angle=180,
+            min_pulse_width=0.0005,
+            max_pulse_width=0.0025,
+            pin_factory=self._factory,
+        )
+        return {name: make_servo(pin) for name, pin in self.config["servos"].items()}
 
 config = load_config()
 controller = ServoController(config)
-controller.act("A")
+for i in ascii_uppercase:
+    controller.act(i)
+    sleep(1)
