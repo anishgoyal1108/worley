cb1ac0438665e22bf5f6bafbc189cf1e8caed77f
Yubo-Cao <cao2006721@gmail.com>
2023-07-09 19:28:39 -0400

    chore(prettier): configure prettier

--- Worley/App.tsx
+++ Worley/App.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StatusBar } from 'expo-status-bar';
+
+function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex h-full justify-center items-center mx-auto`}>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Main" component={Main} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
