24bb227df9eb4f51392269cc78bfd18067102be5
Yubo-Cao <cao2006721@gmail.com>
2023-07-10 18:22:32 -0400

    feat: try to fix WebRTC

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from logging import DEBUG, basicConfig, getLogger
 
 from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaRelay
+from aiortc.contrib.media import MediaRecorder, MediaRelay
 from rich import print
 from rich.logging import RichHandler
 
@@ -16,18 +16,20 @@ basicConfig(level=DEBUG)
 log = getLogger(__name__)
 
 
-def setup_logger(logger):
-    logger.setLevel("INFO")
-    logger.handlers.clear()
-    logger.addHandler(RichHandler())
+def setup_logger(logger, quiet=False):
+    if quiet:
+        logger.setLevel("WARNING")
+    else:
+        logger.setLevel("INFO")
+        logger.handlers.clear()
+        logger.addHandler(RichHandler())
 
 
 setup_logger(log)
-setup_logger(getLogger("aiortc"))
-setup_logger(getLogger("aioice"))
+setup_logger(getLogger("aiortc"), quiet=True)
+setup_logger(getLogger("aioice"), quiet=True)
 
 
-RELAY = MediaRelay()
 pcs = set()
 
 
@@ -37,56 +39,61 @@ async def ping(request):
     return web.json_response({"message": "pong"})
 
 
-async def create_pc() -> tuple[RTCPeerConnection, str]:
+@router.post("/offer")
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    relay = MediaRelay()
+
     pc = RTCPeerConnection()
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
-    return pc, pc_id
 
-
-@router.post("/offer")
-async def offer(request):
-    log.info(f"offer from {request.remote}")
-
-    param = await request.json()
-    offer = RTCSessionDescription(sdp=param["sdp"], type=param["type"])
-
-    pc, pc_id = await create_pc()
-
-    def info(msg, *args):
+    def log_info(msg, *args):
         log.info(pc_id + " " + msg, *args)
 
-    info("Created for %s", request.remote)
+    log_info("Created for %s", request.remote)
+
+    # prepare local media
+    recorder = MediaRecorder(str("./test.wav"))
 
     @pc.on("datachannel")
     def on_datachannel(channel):
-        @channel.on("text")
+        @channel.on("message")
         def on_message(message):
-            info("Data channel message: %s", message)
-            channel.send("pong")
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
 
-    @pc.on("iceconnectionstatechange")
-    async def on_iceconnectionstatechange():
-        info("ICE connection state is %s", pc.iceConnectionState)
-        if pc.iceConnectionState == "failed":
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
             await pc.close()
+            pcs.discard(pc)
 
     @pc.on("track")
     def on_track(track):
-        info("Track %s received", track.kind)
-        pc.addTrack(track)
-        if track.kind != "audio":
-            return
+        log_info("Track %s received", track.kind)
 
+        if track.kind == "audio":
+            log_info("Adding track to recorder")
+            recorder.addTrack(track)
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            await recorder.stop()
+
+    # handle offer
     await pc.setRemoteDescription(offer)
+    await recorder.start()
+
+    # send answer
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)  # type: ignore
 
     return web.json_response(
-        {
-            "sdp": pc.localDescription.sdp,
-            "type": pc.localDescription.type,
-        }
+        {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
     )
 
 
