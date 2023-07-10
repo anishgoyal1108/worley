55c70d862ae8a956afe5d480337712f8f668f4fa
Yubo-Cao <cao2006721@gmail.com>
2023-07-09 22:59:28 -0400

    style(server): refactoring

--- server/main.py
+++ server/main.py
@@ -1,62 +1,37 @@
 import base64
-import os
 from logging import INFO, basicConfig, debug, info
-from queue import Queue
-from tempfile import TemporaryDirectory
 
-import ffmpeg
-import flask
-import numpy as np
-import torch
-import whisper
 from flask import Flask, jsonify, request
+from models import load_speech_to_text, load_vad
 
 basicConfig(level=INFO)
 
-app = flask.Flask(__name__)
-
-info("Loading model...")
-model = whisper.load_model("tiny")
-model = model.to("cuda" if torch.cuda.is_available() else "cpu")
+app = Flask(__name__)
+speech_to_text = load_speech_to_text()
+vad = load_vad()
 
 
-audio_queue = Queue()
-
-
-def _load_audio(buffer, format: str):
-    with TemporaryDirectory() as tmpdir:
-        file_path = os.path.join(tmpdir, "audio." + format.lstrip("."))
-        with open(buffer, "b") as f, open(file_path, "wb") as g:
-            g.write(f.read())
-        out, _ = (
-            ffmpeg.input(file_path, threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=16000)
-            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
-        )
-        return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+@app.route("/asr/handshake", methods=["POST"])
+def session():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No data"}), 400
+    session_id = str(len(sessions))
+    sessions[session_id] = {"interval": data.get("interval", 500)}
+    return jsonify({"session_id": session_id})
 
 
 @app.route("/asr/recognize", methods=["POST"])
 def recognize():
     data = request.json
+    if not data or "audio" not in data:
+        return jsonify({"error": "No audio data"}), 400
+
     audio = _load_audio(
         base64.b64decode(data["audio"]),
         data["format"],
     )
     audio_queue.put(audio)
-
-
-@app.route("/s2t", methods=["POST"])
-def s2t():
-    mp3_base64 = request.json["base64"]
-    mp3_bytes = base64.b64decode(mp3_base64)
-    with TemporaryDirectory() as tmpdir:
-        buffer = os.path.join(tmpdir, "audio.m4a")
-        with open(buffer, "wb") as f:
-            f.write(mp3_bytes)
-        audio = whisper.load_audio(buffer)
-        result = model.transcribe(audio)
-        return jsonify({"result": result})
 
 
 app.run(host="0.0.0.0", debug=True)
---
+++ server/models.py
@@ -0,0 +1,33 @@
+from logging import info
+
+import numpy as np
+import torch
+import whisper
+
+
+def load_speech_to_text(model_size: str = "tiny"):
+    info("Loading whisper...")
+    model = whisper.load_model(model_size)
+    model = model.to("cuda" if torch.cuda.is_available() else "cpu")
+    info("Whisper loaded.")
+
+    def transcribe(audio: np.ndarray):
+        return model.transcribe(audio)
+
+    return transcribe
+
+
+def load_vad():
+    info("Loading sliero...")
+    model, utils = torch.hub.load(
+        repo_or_dir="snakers4/silero-vad",
+        model="silero_vad",
+        force_reload=True,
+    )
+    (get_speech_ts, _, _, _, _, _) = utils
+    info("Sliero loaded.")
+
+    def vad(audio: np.ndarray):
+        return get_speech_ts(audio, model=model, sampling_rate=16000)
+
+    return vad
