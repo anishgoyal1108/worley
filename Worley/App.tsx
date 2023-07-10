d4ae8bd9770d887507a5bc69fdde6da09ad5b2b5
Yubo-Cao <cao2006721@gmail.com>
2023-07-09 20:38:33 -0400

    feat: have MD3 bottom navigation bar

--- Worley/App.tsx
+++ Worley/App.tsx
@@ -1,19 +1,66 @@
 import React from 'react';
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
+import { RecoilRoot } from 'recoil';
 
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Main, Settings } from '@view';
+import { Ionicons } from '@expo/vector-icons';
+import { useSettings } from '@model';
+import { DarkColorScheme, LightColorScheme, Main, Settings } from '@view';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+function AppContent() {
+  const [settings, _] = useSettings();
+  const { Navigator, Screen } = createMaterialBottomTabNavigator();
+
+  let theme = settings.theme === 'dark' ? MD3DarkTheme : MD3LightTheme;
+  let colors = settings.theme === 'dark' ? DarkColorScheme : LightColorScheme;
+
+  return (
+    <PaperProvider
+      theme={{
+        ...theme,
+        colors,
+      }}
+    >
+      <NavigationContainer>
+        <Navigator>
+          <Screen
+            name="Main"
+            component={Main}
+            options={{
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? 'md-home' : 'md-home-outline'}
+                  color={color}
+                  size={22}
+                />
+              ),
+            }}
+          />
+          <Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? 'md-settings' : 'md-settings-outline'}
+                  color={color}
+                  size={22}
+                />
+              ),
+            }}
+          />
+        </Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen name="Main" component={Main} />
-        <Screen name="Settings" component={Settings} />
-      </Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <AppContent />
+    </RecoilRoot>
   );
 }
